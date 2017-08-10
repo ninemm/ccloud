bb2baa95eb6139b980b43ba7bc2cd3c26b1a711d
hx50859042 <hx50859042@126.com>
2017-08-10 23:41:51 +0800

    fix：更新注释

--- ccloud-web-admin/src/main/java/org/ccloud/controller/admin/_ApiController.java
+++ ccloud-web-admin/src/main/java/org/ccloud/controller/admin/_ApiController.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015-2016, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * Copyright (c) 2015-2016, Eric Huang 黄鑫 (ninemm@qq.com).
  *
  * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
  * you may not use this file except in compliance with the License.
