7e3b14466bac0083a646403eb8d9718d0770c443
hx50859042 <hx50859042@126.com>
2017-09-11 10:16:54 +0800

    fix：修复微信端首页不加载地图的bug，调整页面布局

--- ccloud-bi/ccloud-bi-templates/src/main/webapp/templates/datacenter/js/common.js
+++ ccloud-bi/ccloud-bi-templates/src/main/webapp/templates/datacenter/js/common.js
@@ -1,7 +1,7 @@
 
 Utils = {
 	context: '',	
-	devMode: false,
+	devMode: false,	
 	provCacheName: 'PROV_CACHE_NAME',
 	cityCacheName: 'CITY_CACHE_NAME',
 	countryCacheName: 'COUNTRY_CACHE_NAME',
@@ -14,6 +14,9 @@ Utils = {
 	        type: 2,
 	        content: '加载中...'
 	    });
+	},
+	close: function() {
+		layer.closeAll();
 	},
 	planePath: 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z'
 }
@@ -87,6 +90,8 @@ function BaiduMap() {
 					localStorage.provName = provName;
 					localStorage.cityName = cityName;
 					localStorage.countryName = countryName;
+					
+					//console.log(rs.point.lng, rs.point.lat)
 					
 		            $.cookie(Utils.longitudeCache, rs.point.lng, { expires: 7 });
 		            $.cookie(Utils.latitudeCache, rs.point.lat, { expires: 7 });
