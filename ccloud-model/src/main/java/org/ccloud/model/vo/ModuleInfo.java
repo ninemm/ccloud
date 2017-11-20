aeae79b38ebb2bfa1304ca78d3c79962ffa28550
zcjj6 <zcjj6@DESKTOP-6KFDN9J>
2017-11-20 17:56:18 +0800

    1.商品信息录入页面动态删除行时取消验证，编辑时动态添加验证2.角色分配权限岗位分配权限页面修复显示错位BUG 删除多余VO3.组合商品添加组合商品名称与价格字段

--- ccloud-model/src/main/java/org/ccloud/model/vo/ModuleInfo.java
+++ ccloud-model/src/main/java/org/ccloud/model/vo/ModuleInfo.java
@@ -7,7 +7,35 @@ public class ModuleInfo {
 	private String moduleId;
 	private String moduleName;
 	private List<OperationInfo> list;
+	private String systemName;
+	private int systemRowSpan;
+	private String parentModuleName;
+	private int parentRowSpan;
 	
+	public String getSystemName() {
+		return systemName;
+	}
+	public void setSystemName(String systemName) {
+		this.systemName = systemName;
+	}
+	public int getSystemRowSpan() {
+		return systemRowSpan;
+	}
+	public void setSystemRowSpan(int systemRowSpan) {
+		this.systemRowSpan = systemRowSpan;
+	}
+	public String getParentModuleName() {
+		return parentModuleName;
+	}
+	public void setParentModuleName(String parentModuleName) {
+		this.parentModuleName = parentModuleName;
+	}
+	public int getParentRowSpan() {
+		return parentRowSpan;
+	}
+	public void setParentRowSpan(int parentRowSpan) {
+		this.parentRowSpan = parentRowSpan;
+	}
 	public String getModuleId() {
 		return moduleId;
 	}
