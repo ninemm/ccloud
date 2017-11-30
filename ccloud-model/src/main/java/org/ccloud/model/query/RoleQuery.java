3534da20e018704f14df6c44801113fb52df66b0
zcjj6 <zcjj6@DESKTOP-6KFDN9J>
2017-11-30 10:21:57 +0800

    1.分组改变角色增加快捷入口2.用户真实姓名必填

--- ccloud-model/src/main/java/org/ccloud/model/query/RoleQuery.java
+++ ccloud-model/src/main/java/org/ccloud/model/query/RoleQuery.java
@@ -143,18 +143,24 @@ public class RoleQuery extends JBaseQuery {
 	}
 
 	public List<Record> findBydeptAndOperation(String dataArea, String id) {
-		String data = dataArea + "%";
  		StringBuilder fromBuilder = new StringBuilder("select * ");
 		fromBuilder.append("from role r ");
 		fromBuilder.append("left join (SELECT gr.role_id FROM role_operation_rel gr WHERE gr.operation_id = ?) b ");
 		fromBuilder.append("on r.id = b.role_id ");
 		fromBuilder.append("where r.data_area like ?");
-		List<Record> list = Db.find(fromBuilder.toString(), id, data);
+		List<Record> list = Db.find(fromBuilder.toString(), id, dataArea);
 		return list;
 	}
 
 	public List<Role> findByDeptId(String deptId) {
 		return DAO.doFind("dept_id = ?", deptId);
+	}
+
+	public List<Record> findByRoleCheck(String id) {
+		StringBuilder stringBuilder = new StringBuilder("SELECT r.id,r.role_name,a.id as check_status FROM role r LEFT JOIN ");
+		stringBuilder.append("(SELECT * FROM group_role_rel gr WHERE gr.group_id = ?) a ");
+		stringBuilder.append("ON r.id = a.role_id ");
+		return Db.find(stringBuilder.toString(), id);
 	}
 
 }
