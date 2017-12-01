a75f7146b77b2ca384fd32c853904de8ebf07129
chenxuebing2 <445943149@qq.com>
2017-12-01 16:53:10 +0800

    在下级部门时取上级部门的sellerId

--- ccloud-model/src/main/java/org/ccloud/model/query/CustomerQuery.java
+++ ccloud-model/src/main/java/org/ccloud/model/query/CustomerQuery.java
@@ -17,12 +17,9 @@ package org.ccloud.model.query;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import org.ccloud.model.Customer;
-import org.ccloud.utils.StringUtils;
 
-import com.jfinal.kit.StrKit;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Record;
@@ -40,52 +37,13 @@ public class CustomerQuery extends JBaseQuery {
 		return QUERY;
 	}
 
-	public Page<Record> paginate(int pageNumber, int pageSize, String keyword, Map<String, String[]> paraMap,
-			String deptId, String dataArea) {
+	public Page<Record> paginate(int pageNumber, int pageSize, String keyword) {
 
-		String select = "select c.id, c.customer_code, c.customer_name, c.contact, c.mobile, c.customer_kind, c.is_enabled, c.is_archive, "
-				+ " c.prov_name, c.city_name, c.country_name, c.prov_code, c.city_code, c.country_code, c.address, t1.customerTypeNames, t2.realnames";
-		
+		String select = "select * ";
 		StringBuilder fromBuilder = new StringBuilder("from `cc_customer` c ");
 
 		LinkedList<Object> params = new LinkedList<Object>();
-		boolean needWhere = true;
 
-		fromBuilder.append(" LEFT JOIN ( SELECT c1.id, GROUP_CONCAT(ct. NAME) AS customerTypeNames ");
-		fromBuilder.append(
-				" FROM cc_customer c1 LEFT JOIN cc_customer_join_customer_type cjct ON c1.id = cjct.customer_id ");
-		fromBuilder.append(" LEFT JOIN cc_customer_type ct ON cjct.customer_type_id = ct.id ");
-		needWhere = appendIfNotEmpty(fromBuilder, "cjct.customer_type_id",
-				StringUtils.getArrayFirst(paraMap.get("customerTypeId")), params, needWhere);
-		needWhere = appendIfNotEmpty(fromBuilder, "ct.dept_id", deptId, params, needWhere);
-		needWhere = appendIfNotEmptyWithLike(fromBuilder, "ct.data_area", dataArea, params, needWhere);
-		fromBuilder.append(" GROUP BY c1.id ");
-		fromBuilder.append(" ) t1 ON c.id = t1.id");
-
-		needWhere = true;
-		if (StrKit.isBlank(deptId)) {
-			fromBuilder.append(" LEFT JOIN (SELECT c2.id, GROUP_CONCAT(u.realname) AS realnames ");
-		} else {
-			fromBuilder.append(" JOIN (SELECT c2.id, GROUP_CONCAT(u.realname) AS realnames ");
-		}
-		fromBuilder.append(" FROM cc_customer c2 JOIN cc_user_join_customer ujc ON c2.id = ujc.customer_id ");
-		fromBuilder.append(" JOIN USER u ON ujc.user_id = u.id ");
-		needWhere = appendIfNotEmpty(fromBuilder, "ujc.dept_id", deptId, params, needWhere);
-		needWhere = appendIfNotEmptyWithLike(fromBuilder, "ujc.data_area", dataArea, params, needWhere);
-		fromBuilder.append(" GROUP BY c2.id ");
-		fromBuilder.append(" ) t2 ON c.id = t2.id ");
-
-		needWhere = true;
-		needWhere = appendIfNotEmptyWithLike(fromBuilder, "c.customer_name", keyword, params, needWhere);
-		needWhere = appendIfNotEmpty(fromBuilder, "c.customer_kind",
-				StringUtils.getArrayFirst(paraMap.get("customerKind")), params, needWhere);
-
-		needWhere = appendIfNotEmpty(fromBuilder, "c.is_enabled", StringUtils.getArrayFirst(paraMap.get("isEnabled")),
-				params, needWhere);
-		needWhere = appendIfNotEmpty(fromBuilder, "c.is_archive", StringUtils.getArrayFirst(paraMap.get("isArchive")),
-				params, needWhere);
-
-		fromBuilder.append(" GROUP BY c.id ");
 		fromBuilder.append(" order by c.create_date ");
 
 		if (params.isEmpty())
@@ -109,7 +67,7 @@ public class CustomerQuery extends JBaseQuery {
 			}
 		});
 	}
-	
+
 	public Customer findByCustomerNameAndMobile(String customerName, String mobile) {
 		return DAO.doFindFirst("customer_name = ? and mobile = ?", customerName, mobile);
 	}
@@ -142,10 +100,10 @@ public class CustomerQuery extends JBaseQuery {
 	public List<Customer> findByUserId(String id) {
 		StringBuilder sql = new StringBuilder("SELECT cc.* FROM cc_customer cc ");
 		sql.append("RIGHT JOIN cc_user_join_customer a ON cc.id = a.customer_id ");
-		sql.append("WHERE a.user_id= ? ");		
+		sql.append("WHERE a.user_id= ? ");
 		return DAO.find(sql.toString(), id);
 	}
-	
+
 	public List<Customer> getToDo(String username) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("SELECT c.*, a.ID_ taskId, a.NAME_ taskName, a.ASSIGNEE_ assignee, a.CREATE_TIME_ createTime");
@@ -156,7 +114,7 @@ public class CustomerQuery extends JBaseQuery {
 		return DAO.find(sb.toString(), username);
 	}
 
-	public List<Record> getCustomerIdName(){
+	public List<Record> getCustomerIdName() {
 		StringBuilder fromBuilder = new StringBuilder("SELECT id,customer_name AS text FROM cc_customer");
 		List<Record> list = Db.find(fromBuilder.toString());
 		return list;
