d05921a953999c9874ca1896ade5fb054e55023d
yangfan <15549459554@163.com>
2017-12-05 11:09:15 +0800

    jmodel提交

--- ccloud-model-core/src/main/java/org/ccloud/model/core/JModel.java
+++ ccloud-model-core/src/main/java/org/ccloud/model/core/JModel.java
@@ -175,13 +175,8 @@ public class JModel<M extends JModel<M>> extends Model<M> {
 	
 	public boolean saveOrUpdateWithoutDate() {
 		if (null == get(getPrimaryKey())) {
-			set("id", StrKit.getRandomUUID());
-//			set("create_date", new Date());
-			set("created", new Date());
 			return this.save();
 		}
-//		set("modify_date", new Date());
-		set("modified", new Date());
 		return this.update();
 	}
 
