1249238cca7fed50e00ceba6281483815f68ebfb
Larry <Larry@DESKTOP-QIDTT2S>
2017-12-07 21:39:46 +0800

    出库明细

--- ccloud-web-admin/src/main/java/org/ccloud/controller/admin/_InventoryDetailController.java
+++ ccloud-web-admin/src/main/java/org/ccloud/controller/admin/_InventoryDetailController.java
@@ -51,7 +51,7 @@ public class _InventoryDetailController extends JBaseCRUDController<InventoryDet
             keyword = StringUtils.urlDecode(keyword);
             setAttr("k", keyword);
         }
-		Page<InventoryDetail> page = InventoryDetailQuery.me()._in_paginate(getPageNumber(), getPageSize(),keyword,"cid.id",user.getId(),user.getDataArea());
+		Page<InventoryDetail> page = InventoryDetailQuery.me()._in_paginate(getPageNumber(), getPageSize(),keyword,"cid.create_date",user.getId(),user.getDataArea());
 		Map<String, Object> map = ImmutableMap.of("total", page.getTotalRow(), "rows", page.getList());
         renderJson(map);
 	}
@@ -67,7 +67,7 @@ public class _InventoryDetailController extends JBaseCRUDController<InventoryDet
             keyword = StringUtils.urlDecode(keyword);
             setAttr("k", keyword);
         }
-		Page<InventoryDetail> page = InventoryDetailQuery.me()._out_paginate(getPageNumber(), getPageSize(),keyword,"cid.id",user.getId(),user.getDataArea());
+		Page<InventoryDetail> page = InventoryDetailQuery.me()._out_paginate(getPageNumber(), getPageSize(),keyword,"cid.create_date",user.getId(),user.getDataArea());
 		Map<String, Object> map = ImmutableMap.of("total", page.getTotalRow(), "rows", page.getList());
         renderJson(map);
 	}
