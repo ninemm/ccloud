b250689a97920c18c4a1d925546f8f8dbb86a224
hx50859042 <hx50859042@126.com>
2017-12-11 00:05:04 +0800

    fix：完善用户反馈功能

--- ccloud-web-front/src/main/java/org/ccloud/front/controller/FeedbackController.java
+++ ccloud-web-front/src/main/java/org/ccloud/front/controller/FeedbackController.java
@@ -15,9 +15,6 @@
  */
 package org.ccloud.front.controller;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.util.List;
 
 import org.ccloud.Consts;
@@ -28,16 +25,11 @@ import org.ccloud.model.query.FeedbackQuery;
 import org.ccloud.model.vo.ImageJson;
 import org.ccloud.route.RouterMapping;
 import org.ccloud.utils.DateUtils;
-import org.ccloud.utils.FileUtils;
-import org.joda.time.DateTime;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.beust.jcommander.internal.Lists;
-import com.jfinal.kit.Base64Kit;
-import com.jfinal.kit.LogKit;
-import com.jfinal.kit.PathKit;
 import com.jfinal.kit.Ret;
 import com.jfinal.kit.StrKit;
 import com.jfinal.plugin.activerecord.Page;
@@ -132,38 +124,4 @@ public class FeedbackController extends BaseFrontController {
 		renderAjaxResultForSuccess("success", ret);
 	}
 	
-	private String upload(String blobImage) {
-		if (StrKit.notBlank(blobImage)) {
-			
-			String webRoot = PathKit.getWebRootPath();
-			String uuid = StrKit.getRandomUUID();
-			StringBuilder newFileName = new StringBuilder(webRoot).append(File.separator).append("attachment")
-					.append(File.separator).append(DateTime.now().toString("yyyyMMdd")).append(File.separator).append(uuid)
-					.append(".jpeg");
-
-			File newfile = new File(newFileName.toString());
-
-			if (!newfile.getParentFile().exists()) {
-				newfile.getParentFile().mkdirs();
-			}
-			
-			try (OutputStream os = new FileOutputStream(newfile)) {
-				blobImage = blobImage.substring(blobImage.indexOf(",") + 1);
-				byte[] b = Base64Kit.decode(blobImage);
-				for (int i = 0; i < b.length; ++i) {
-		            if (b[i] < 0) {//调整异常数据
-		                b[i] += 256;
-		            }
-		        }
-				
-				os.write(b);
-				os.close();
-			} catch (Exception e) {
-				LogKit.error("上传反馈图片失败!");
-				return null;
-			}
-			return FileUtils.removePrefix(newfile.getAbsolutePath(), webRoot);
-		}
-		return null;
-	}
 }
