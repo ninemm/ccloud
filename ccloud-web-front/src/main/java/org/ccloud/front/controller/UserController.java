24d22a14d367bc3806a4d359154951cc7647d958
Administrator <Administrator@USER-UDNOQD3CCT>
2017-12-13 11:44:56 +0800

    1.客户拜访

--- ccloud-web-front/src/main/java/org/ccloud/front/controller/UserController.java
+++ ccloud-web-front/src/main/java/org/ccloud/front/controller/UserController.java
@@ -2,7 +2,11 @@ package org.ccloud.front.controller;
 
 import java.math.BigInteger;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
@@ -12,11 +16,16 @@ import org.ccloud.core.BaseFrontController;
 import org.ccloud.interceptor.UserInterceptor;
 import org.ccloud.message.Actions;
 import org.ccloud.message.MessageKit;
+import org.ccloud.model.Customer;
+import org.ccloud.model.CustomerVisit;
+import org.ccloud.model.Dict;
 import org.ccloud.model.SmsCode;
 import org.ccloud.model.User;
+import org.ccloud.model.query.DictQuery;
 import org.ccloud.model.query.SellerQuery;
 import org.ccloud.model.query.SmsCodeQuery;
 import org.ccloud.model.query.UserQuery;
+import org.ccloud.model.vo.ImageJson;
 import org.ccloud.route.RouterMapping;
 import org.ccloud.shiro.CaptchaUsernamePasswordToken;
 import org.ccloud.utils.CookieUtils;
@@ -24,6 +33,9 @@ import org.ccloud.utils.DataAreaUtil;
 import org.ccloud.utils.EncryptUtils;
 import org.ccloud.utils.StringUtils;
 
+import com.alibaba.fastjson.JSON;
+import com.beust.jcommander.internal.Lists;
+import com.google.common.collect.Maps;
 import com.jfinal.aop.Clear;
 import com.jfinal.core.ActionKey;
 import com.jfinal.kit.Ret;
@@ -253,5 +265,142 @@ public class UserController extends BaseFrontController{
 		} catch (AuthenticationException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void inventory() {
+		//User loginUser = getSessionAttr(Consts.SESSION_LOGINED_USER);
+		//String wareHouseId = getPara("mobile");
+		//String productName = getPara("product");
+		String sellerId = getPara("sellerId");
+		String selDataArea = getSessionAttr(Consts.SESSION_SELECT_DATAAREA);
+		String deptId = "";
+		//Page<Record> inventoryList = InventoryQuery.me().findDetailByApp(getPageNumber(), getPageSize(),"","",sellerId,deptDataArea,deptId);
+		Page<Record> inventoryList = new Page<Record>();
+		List<GoodsType> goodsTypeList = new ArrayList<GoodsType>();
+		if(StrKit.notBlank(selDataArea)) {
+			inventoryList = InventoryQuery.me().findDetailByParams("","", sellerId, "", deptId, selDataArea,"",getPageNumber(), getPageSize());
+			goodsTypeList = GoodsTypeQuery.me().findGoodsType(selDataArea);
+		}
+		setAttr("inventoryList", inventoryList);
+		setAttr("goodsTypeList", goodsTypeList);
+		render("inventory.html");
+	}
+	
+	public void appLoadRegionAndProductType() {
+		//User loginUser = getSessionAttr(Consts.SESSION_LOGINED_USER);
+		String selDataArea = getSessionAttr(Consts.SESSION_SELECT_DATAAREA);
+		String goodsType = getPara("goodsType");
+		String queryType = getPara("queryType");
+		List<Map<String, Object>> regionList = new ArrayList<>();
+		if(!queryType.equals("productType")) {
+			//String deptDataArea = loginUser!=null?DataAreaUtil.getUserDeptDataArea(loginUser.getDataArea()):"";
+			List<Seller> sellerList = new ArrayList<Seller>();
+			if(StrKit.notBlank(selDataArea)) {
+				sellerList = SellerQuery.me().findSellerRegion(selDataArea);
+			}
+			Map<String, Object> region = new HashMap<>();
+			region.put("title", "全部");
+			region.put("value", "");
+			regionList.add(region);
+			for(Seller seller : sellerList) {
+				Map<String, Object> item = new HashMap<>();
+				item.put("title", seller.getSellerName());
+				item.put("value", seller.getId());
+				regionList.add(item);
+			}
+		}
+		//List<GoodsType> productTypeList = GoodsTypeQuery.me().findProductType(deptDataArea+"%");
+		goodsType = (StrKit.notBlank(goodsType))?goodsType:"";
+		List<Product> productList = ProductQuery.me().findAllProduct(goodsType);
+		List<Map<String, Object>> typeList = new ArrayList<>();
+		Map<String, Object> type = new HashMap<>();
+		type.put("title", "全部");
+		type.put("value", "");
+		typeList.add(type);
+		for(Product product : productList) {
+			Map<String, Object> item = new HashMap<>();
+			item.put("title", product.getName());
+			item.put("value", product.getId());
+			typeList.add(item);
+		}
+		Map<String, List<Map<String, Object>>> data = ImmutableMap.of("region", regionList, "productType", typeList);
+		renderJson(data);
+	}
+	
+	public void appLoadFollowUpData() {
+		//User loginUser = getSessionAttr(Consts.SESSION_LOGINED_USER);
+		String selDataArea = getSessionAttr(Consts.SESSION_SELECT_DATAAREA);
+		String search = getPara("search");
+		int pageNumber = Integer.parseInt(getPara("pageNumber"));
+		int pageSize = Integer.parseInt(getPara("pageSize"));
+		String sellerId = getPara("region");
+		String productType = getPara("productType");
+		productType = productType.equals("全部")?"":productType;
+		String isOrdered = getPara("isOrdered");
+		if(isOrdered==null)isOrdered="00";
+		String goodsType = getPara("goodsType");
+		goodsType = goodsType.equals("00")||goodsType==null?"":goodsType;
+		//String deptDataArea = loginUser!=null?DataAreaUtil.getUserDeptDataArea(loginUser.getDataArea()):"";
+		String deptId = "";
+		Page<Record> inventoryList = new Page<Record>();
+		StringBuilder inventoryHtml = new StringBuilder("<div class=\"weui-loadmore weui-loadmore_line\"><span class=\"weui-loadmore__tips\"  style=\"float: inherit;\">暂无数据</span></div>");
+		if(StrKit.notBlank(selDataArea)) {
+			inventoryList = InventoryQuery.me().findDetailByParams(search,goodsType, sellerId, productType, deptId, selDataArea,isOrdered,pageNumber,pageSize);
+			if(inventoryList.getList().size()>0||pageNumber>1) {
+				inventoryHtml.delete(0, inventoryHtml.length());	
+			}
+			for (Record inventory : inventoryList.getList()) {
+				inventoryHtml.append("<div class=\"product_detail\">");
+				inventoryHtml.append("<div class=\"inventory_name\">"+inventory.getStr("name")+"</div>");
+				//期初期末结存 未定,暂时不做统计。
+				//inventoryHtml.append("<div class=\"weui-flex\"><div class=\"weui-flex__item\">期初结存：<span>"+inventory.getStr("in_count")+"</span></div><div class=\"weui-flex__item\">期末结存：<span>"+inventory.getStr("out_count")+"</span></div></div>");
+				inventoryHtml.append("<div class=\"weui-flex\"><div class=\"weui-flex__item\">出库：<span class=\"green-button\">"+inventory.getStr("in_count")+"</span></div><div class=\"weui-flex__item\">入库：<span class=\"yellow-button\">"+inventory.getStr("out_count")+"</span></div></div>");
+				inventoryHtml.append("<div class=\"weui-flex\"><div class=\"weui-flex__item\">库存：<span class=\"blue-button\">"+inventory.getStr("balance_count")+"</span></div><div class=\"weui-flex__item\">在途：<span>"+inventory.getStr("afloat_count")+"</span></div></div>");
+				inventoryHtml.append("<div><i class=\"icon-map-pin blue ft16\"></i>&nbsp;&nbsp;"+inventory.getStr("seller_name")+"</div>");
+				inventoryHtml.append("</div>\n");
+			}
+		}
+		Map<String, Object> map = new HashMap<>();
+		map.put("inventoryHtml", inventoryHtml.toString());
+		map.put("totalRow", inventoryList.getTotalRow());
+		map.put("totalPage", inventoryList.getTotalPage());
+		renderJson(map);
+		//renderAjaxResultForSuccess("success",JSON.toJSON(inventoryList));
+	}	
+	
+	// 用户新增拜访页面显示
+	public void visitAdd() {
+	    String user_id = "1f797c5b2137426093100f082e234c14";
+	    String data_area = "0010010016410";
+	    
+	    List<Record> customer_list = UserQuery.me().getCustomerInfoByUserId(user_id,data_area);
+	    List<Dict> problem_list = DictQuery.me().findByCode("visit");
+	    
+	    setAttr("customer",JSON.toJSONString(customer_list));
+	    setAttr("problem",JSON.toJSONString(problem_list));
+		
+		render("user_visitAdd.html");
+	}
+	
+	// 用户新增拜访保存
+	public void save() {
+		
+		 CustomerVisit customerVisit = getModel(CustomerVisit.class);
+		 String user_id= "1f797c5b2137426093100f082e234c14";
+		 String data_area = "0010010016410";
+		 String picJson = getPara("pic");
+		 String seller_customer_id = getPara("customer_id");
+		 String question_type = getPara("problem_id");
+		 String location = getPara("location");
+		 String mobile = getPara("mobile");
+		 String sex = getPara("sex");
+		 String question_desc = getPara("question_desc");
+		 
+		 String visit_id = StrKit.getRandomUUID();
+		 Date date = new Date();
+		 customerVisit.set("id", visit_id);
+		 customerVisit.set("user_id", user_id);
+		 customerVisit.set("seller_customer_id", seller_customer_id);
+		 
 	}
 }
