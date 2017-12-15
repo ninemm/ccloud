d2a12d433663d9074b3c8f242272b77c4a67d0f6
hx50859042 <hx50859042@126.com>
2017-12-15 12:05:55 +0800

    add: APP端新增多账户切换

--- ccloud-web-front/src/main/java/org/ccloud/front/controller/UserController.java
+++ ccloud-web-front/src/main/java/org/ccloud/front/controller/UserController.java
@@ -150,7 +150,7 @@ public class UserController extends BaseFrontController {
 				setSessionAttr(Consts.SESSION_SELLER_CODE, map.get("seller_code"));
 			}
 			// 获取用户权限
-			init(user.getUsername(), user.getPassword(), true);
+			initUserRole(user.getUsername(), user.getPassword(), true);
 			
 			if (this.isAjaxRequest()) {
 				renderAjaxResultForSuccess("登录成功");
@@ -195,6 +195,26 @@ public class UserController extends BaseFrontController {
 	
 	public void change() {
 		
+		User curUser = initSellerAccount();
+		
+		if (curUser == null) {
+			if (isAjaxRequest())
+				renderAjaxResultForError("切换账号失败");
+			else
+				renderError(404);
+			return ;
+		}
+		
+		initUserRole(curUser.getUsername(), curUser.getPassword(), true);
+		
+		if (isAjaxRequest()) {
+			renderAjaxResultForSuccess("切换账号成功");
+			return ;
+		}
+		redirect("/"); 
+	}
+	
+	private User initSellerAccount() {
 		String mobile = getPara("mobile");
 		String openid = getPara("openid");
 		String sellerId = getPara("sellerId");
@@ -220,15 +240,8 @@ public class UserController extends BaseFrontController {
 			}
 		}
 		
-		if (curUser == null) {
-			renderError(404);
-			return ;
-		}
-		
-		init(curUser.getUsername(), curUser.getPassword(), true);
-		
-		redirect("/"); 
-	}
+		return curUser;
+	} 
 	
 	public void checkMobile() {
 		
@@ -279,7 +292,7 @@ public class UserController extends BaseFrontController {
 					}
 					
 					// 获取用户权限
-					init(user.getUsername(), user.getPassword(), true);
+					initUserRole(user.getUsername(), user.getPassword(), true);
 
 					List<Department> tmpList = DepartmentQuery.me().findAllParentDepartmentsBySubDeptId(user.getDepartmentId());
 					if (tmpList.size() > 0) {
@@ -300,7 +313,7 @@ public class UserController extends BaseFrontController {
 		renderAjaxResultForError(ret.getStr("message"));
 	}
 
-	private void init(String username, String password, Boolean rememberMe) {
+	private void initUserRole(String username, String password, Boolean rememberMe) {
 		
 		Subject subject = SecurityUtils.getSubject();
 		CaptchaUsernamePasswordToken token = new CaptchaUsernamePasswordToken(username, password, rememberMe, "", "");
@@ -323,106 +336,4 @@ public class UserController extends BaseFrontController {
 			e.printStackTrace();
 		}
 	}
-	
-	public void inventory() {
-		//User loginUser = getSessionAttr(Consts.SESSION_LOGINED_USER);
-		//String wareHouseId = getPara("mobile");
-		//String productName = getPara("product");
-		String sellerId = getPara("sellerId");
-		String selDataArea = getSessionAttr(Consts.SESSION_SELECT_DATAAREA);
-		String deptId = "";
-		//Page<Record> inventoryList = InventoryQuery.me().findDetailByApp(getPageNumber(), getPageSize(),"","",sellerId,deptDataArea,deptId);
-		Page<Record> inventoryList = new Page<Record>();
-		List<GoodsType> goodsTypeList = new ArrayList<GoodsType>();
-		if(StrKit.notBlank(selDataArea)) {
-			inventoryList = InventoryQuery.me().findDetailByParams("","", sellerId, "", deptId, selDataArea,"",getPageNumber(), getPageSize());
-			goodsTypeList = GoodsTypeQuery.me().findGoodsType(selDataArea);
-		}
-		setAttr("inventoryList", inventoryList);
-		setAttr("goodsTypeList", goodsTypeList);
-		render("inventory.html");
-	}
-	
-	public void appLoadRegionAndProductType() {
-		//User loginUser = getSessionAttr(Consts.SESSION_LOGINED_USER);
-		String selDataArea = getSessionAttr(Consts.SESSION_SELECT_DATAAREA);
-		String goodsType = getPara("goodsType");
-		String queryType = getPara("queryType");
-		List<Map<String, Object>> regionList = new ArrayList<>();
-		if(!queryType.equals("productType")) {
-			//String deptDataArea = loginUser!=null?DataAreaUtil.getUserDeptDataArea(loginUser.getDataArea()):"";
-			List<Seller> sellerList = new ArrayList<Seller>();
-			if(StrKit.notBlank(selDataArea)) {
-				sellerList = SellerQuery.me().findSellerRegion(selDataArea);
-			}
-			Map<String, Object> region = new HashMap<>();
-			region.put("title", "全部");
-			region.put("value", "");
-			regionList.add(region);
-			for(Seller seller : sellerList) {
-				Map<String, Object> item = new HashMap<>();
-				item.put("title", seller.getSellerName());
-				item.put("value", seller.getId());
-				regionList.add(item);
-			}
-		}
-		//List<GoodsType> productTypeList = GoodsTypeQuery.me().findProductType(deptDataArea+"%");
-		goodsType = (StrKit.notBlank(goodsType))?goodsType:"";
-		List<Product> productList = ProductQuery.me().findAllProduct(goodsType);
-		List<Map<String, Object>> typeList = new ArrayList<>();
-		Map<String, Object> type = new HashMap<>();
-		type.put("title", "全部");
-		type.put("value", "");
-		typeList.add(type);
-		for(Product product : productList) {
-			Map<String, Object> item = new HashMap<>();
-			item.put("title", product.getName());
-			item.put("value", product.getId());
-			typeList.add(item);
-		}
-		Map<String, List<Map<String, Object>>> data = ImmutableMap.of("region", regionList, "productType", typeList);
-		renderJson(data);
-	}
-	
-	public void appLoadFollowUpData() {
-		//User loginUser = getSessionAttr(Consts.SESSION_LOGINED_USER);
-		String selDataArea = getSessionAttr(Consts.SESSION_SELECT_DATAAREA);
-		String search = getPara("search");
-		int pageNumber = Integer.parseInt(getPara("pageNumber"));
-		int pageSize = Integer.parseInt(getPara("pageSize"));
-		String sellerId = getPara("region");
-		String productType = getPara("productType");
-		productType = productType.equals("全部")?"":productType;
-		String isOrdered = getPara("isOrdered");
-		if(isOrdered==null)isOrdered="00";
-		String goodsType = getPara("goodsType");
-		goodsType = goodsType.equals("00")||goodsType==null?"":goodsType;
-		//String deptDataArea = loginUser!=null?DataAreaUtil.getUserDeptDataArea(loginUser.getDataArea()):"";
-		String deptId = "";
-		Page<Record> inventoryList = new Page<Record>();
-		StringBuilder inventoryHtml = new StringBuilder("<div class=\"weui-loadmore weui-loadmore_line\"><span class=\"weui-loadmore__tips\"  style=\"float: inherit;\">暂无数据</span></div>");
-		if(StrKit.notBlank(selDataArea)) {
-			inventoryList = InventoryQuery.me().findDetailByParams(search,goodsType, sellerId, productType, deptId, selDataArea,isOrdered,pageNumber,pageSize);
-			if(inventoryList.getList().size()>0||pageNumber>1) {
-				inventoryHtml.delete(0, inventoryHtml.length());	
-			}
-			for (Record inventory : inventoryList.getList()) {
-				inventoryHtml.append("<div class=\"product_detail\">");
-				inventoryHtml.append("<div class=\"inventory_name\" style=\"font-size: 0.7rem;\">"+inventory.getStr("name")+"</div>");
-				//期初期末结存 未定,暂时不做统计。
-				//inventoryHtml.append("<div class=\"weui-flex\"><div class=\"weui-flex__item\">期初结存：<span>"+inventory.getStr("in_count")+"</span></div><div class=\"weui-flex__item\">期末结存：<span>"+inventory.getStr("out_count")+"</span></div></div>");
-				inventoryHtml.append("<div class=\"weui-flex\" style=\"margin:0.1rem;\"><div class=\"weui-flex__item\">出库：<span class=\"green-button\">"+inventory.getStr("in_count")+"</span></div><div class=\"weui-flex__item\">入库：<span class=\"yellow-button\">"+inventory.getStr("out_count")+"</span></div></div>");
-				//暂时隐藏在途量 <div class=\"weui-flex__item\">在途：<span>"+inventory.getStr("afloat_count")+"</span></div>
-				inventoryHtml.append("<div class=\"weui-flex\" style=\"margin:0.1rem;\"><div class=\"weui-flex__item\">库存：<span class=\"blue-button\">"+inventory.getStr("balance_count")+"</span></div><div class=\"weui-flex__item\"></div></div>");
-				inventoryHtml.append("<div><i class=\"icon-map-pin blue ft16\"></i>&nbsp;&nbsp;"+inventory.getStr("seller_name")+"</div>");
-				inventoryHtml.append("</div>\n");
-			}
-		}
-		Map<String, Object> map = new HashMap<>();
-		map.put("inventoryHtml", inventoryHtml.toString());
-		map.put("totalRow", inventoryList.getTotalRow());
-		map.put("totalPage", inventoryList.getTotalPage());
-		renderJson(map);
-		//renderAjaxResultForSuccess("success",JSON.toJSON(inventoryList));
-	}	
 }
