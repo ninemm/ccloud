2cd77d1af637acde2af8a689897dd75eab000f46
yangliyang <948487671@qq.com>
2017-12-20 19:27:03 +0800

    批量出库和已打未打打印之间控制

--- ccloud-web-admin/src/main/java/org/ccloud/controller/admin/_SalesOutstockController.java
+++ ccloud-web-admin/src/main/java/org/ccloud/controller/admin/_SalesOutstockController.java
@@ -174,6 +174,26 @@ public class _SalesOutstockController extends JBaseCRUDController<SalesOrder> {
 
 	}	
 	
+	
+//	@RequiresPermissions("/admin/salesOutstock/check")
+//	public void batchOutStock() {
+//
+//		String outstockId = getPara("outstockId");
+//		String[] outId = outstockId.split(",");
+//		String date = getPara("oStockDate");
+//		String remark = getPara("remark");
+//		User user = getSessionAttr(Consts.SESSION_LOGINED_USER);
+//		String sellerId = getSessionAttr("sellerId");
+//		String sellerCode = getSessionAttr("sellerCode");
+//		boolean isSave = this.out(paraMap, user, sellerId, sellerCode);
+//        if (isSave) {
+//        	renderAjaxResultForSuccess("出库成功");
+//        } else {
+//        	renderAjaxResultForError("出库失败!");
+//        }
+//
+//	}
+	
 	public void recordPrintInfo() {
 		String outstockId = getPara("outstockId");
 		String[] outId = outstockId.split(",");
