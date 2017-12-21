89ec548792ab0321721f233baad6a48602b4dac1
yangfan <15549459554@163.com>
2017-12-21 14:33:41 +0800

    客户详情报表

--- ccloud-commons/src/main/java/org/ccloud/Consts.java
+++ ccloud-commons/src/main/java/org/ccloud/Consts.java
@@ -156,6 +156,8 @@ public class Consts {
 	
 	public static final String ROLE_CODE_007 = "007";//财务
 	public static final String ROLE_CODE_020 = "020";//账务
+	public static final String ROLE_CODE_010 = "010";//业务员
+	
 	
 	public static final String OPERATE_HISTORY_TITLE_ORDER_REVIEW= "订单审核";
 	public static final String OPERATE_HISTORY_TITLE_ORDER_PRINT= "订单打印";
