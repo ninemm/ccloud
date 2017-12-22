a622823b4d66aed39d627869a5ae35fb31332425
zcjj6 <zcjj6@DESKTOP-6KFDN9J>
2017-12-22 21:12:43 +0800

    1.合并

--- ccloud-web-front/src/main/java/org/ccloud/front/controller/CustomerController.java
+++ ccloud-web-front/src/main/java/org/ccloud/front/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.ccloud.Consts;
 import org.ccloud.core.BaseFrontController;
