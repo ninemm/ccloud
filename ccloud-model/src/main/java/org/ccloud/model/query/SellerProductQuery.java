466f9e21179a56903842a0a3cf609df565e1c2b3
chenxuebing2 <445943149@qq.com>
2017-12-23 12:20:18 +0800

    商品描述

--- ccloud-model/src/main/java/org/ccloud/model/query/SellerProductQuery.java
+++ ccloud-model/src/main/java/org/ccloud/model/query/SellerProductQuery.java
@@ -137,7 +137,7 @@ public class SellerProductQuery extends JBaseQuery {
 	
 	public List<Record> findProductListForApp(String sellerId, String keyword) {
 		StringBuilder fromBuilder = new StringBuilder(
-				" SELECT sp.id AS sell_product_id, sp.product_id, sp.custom_name, sp.store_count, sp.price, p.convert_relate, p.big_unit, p.small_unit, t1.valueName, g.`name` AS goodsName, gc.`name` AS categoryName, gt.`id` as typeId, gt.`name` as typeName ");
+				" SELECT sp.id AS sell_product_id, sp.product_id, sp.custom_name, sp.store_count, sp.price, p.convert_relate, p.big_unit, p.small_unit, p.description, t1.valueName, g.`name` AS goodsName, gc.`name` AS categoryName, gt.`id` as typeId, gt.`name` as typeName ");
 		fromBuilder.append(" FROM cc_seller_product sp JOIN cc_product p ON sp.product_id = p.id ");
 		fromBuilder.append(" LEFT JOIN (SELECT sv.id, cv.product_set_id, GROUP_CONCAT(sv.`name`) AS valueName FROM cc_goods_specification_value sv RIGHT JOIN cc_product_goods_specification_value cv ON cv.goods_specification_value_set_id = sv.id GROUP BY cv.product_set_id ) t1 ON t1.product_set_id = p.id ");
 		fromBuilder.append(" JOIN cc_goods g ON p.goods_id = g.id JOIN cc_goods_category gc ON g.goods_category_id = gc.id JOIN cc_goods_type gt on g.goods_type_id = gt.id ");
