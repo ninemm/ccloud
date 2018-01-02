e97ca3d35dbb6218fd8b747e05f025daf001a823
chenxuebing2 <445943149@qq.com>
2018-01-02 14:50:22 +0800

    下单修改价格开关

--- ccloud-commons/src/main/java/org/ccloud/Consts.java
+++ ccloud-commons/src/main/java/org/ccloud/Consts.java
@@ -146,6 +146,7 @@ public class Consts {
 	public static final String RECEIVABLES_OBJECT_TYPE_SUPPLIER = "supplier"; //应收账款供应商类型
 	
 	public static final String OPTION_SELLER_STORE_CHECK = "seller_store_check_";//经销商检查库存开关设置
+	public static final String OPTION_WEB_ORDER_PRICE_EDIT = "web_order_price_edit_";//下单修改价格开关设置
 	public static final String OPTION_WEB_PROCEDURE_REVIEW = "web_procedure_review_";//订单审核流程开关设置
 	public static final String OPTION_WEB_PROCEDURE_REVIEW_EDIT = "web_procedure_review_edit_";//订单审核修改开关设置
 	public static final String OPTION_WEB_PROC_CUSTOMER_REVIEW = "web_proc_customer_review_";//客户流程审核开关设置
