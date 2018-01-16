ffe1e5ac73e7146013d82ed7734265a49980e879
hx50859042 <hx50859042@126.com>
2018-01-16 23:03:10 +0800

    add: 新增定时任务插件

--- ccloud-web-core/src/main/java/org/ccloud/wechat/WechatMessageController.java
+++ ccloud-web-core/src/main/java/org/ccloud/wechat/WechatMessageController.java
@@ -89,7 +89,6 @@ public class WechatMessageController extends MsgController {
 		if (StringUtils.areNotBlank(appId, appSecret)) {
 			ApiResult result = WechatApi.getOpenId(appId, appSecret, code);
 			if (result != null) {
-				System.err.println(result.getJson());
 				getRequest().getSession().setAttribute(Consts.SESSION_WECHAT_USER, result.getJson());
 				//setSessionAttr(Consts.SESSION_WECHAT_USER, result.getJson());
 				this.setSessionAttr(Consts.SESSION_WECHAT_USER, result.getJson());
