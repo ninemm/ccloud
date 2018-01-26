87f348973f4794a09c7bacf16c96ba21ae40c313
Larry <Larry@DESKTOP-QIDTT2S>
2018-01-26 18:35:30 +0800

    后台活动显示

--- ccloud-model/src/main/java/org/ccloud/model/query/ActivityQuery.java
+++ ccloud-model/src/main/java/org/ccloud/model/query/ActivityQuery.java
@@ -52,7 +52,7 @@ public class ActivityQuery extends JBaseQuery {
 	}
 
 	public Page<Record> paginate(int pageNumber, int pageSize, String keyword,String startDate, String endDate,String sellerId) {
-		String select = "select ca.*,(SELECT COUNT(*) from cc_activity_apply where activity_id = ca.id and `status` !="+Consts.ACTIVITY_APPLY_STATUS_REJECT+") AS activityApplyNum,ct.name as customerTypeName ";
+		String select = "select ca.*,(SELECT COUNT(*) from cc_activity_apply where activity_id = ca.id and `status` not in ("+Consts.ACTIVITY_APPLY_STATUS_REJECT+","+Consts.ACTIVITY_APPLY_STATUS_CANCEL+") AS activityApplyNum,ct.name as customerTypeName ";
 		StringBuilder fromBuilder = new StringBuilder("from `cc_activity` ca ");
 		fromBuilder.append(" LEFT JOIN cc_customer_type ct on ct.id=ca.customer_type");
 		LinkedList<Object> params = new LinkedList<Object>();
