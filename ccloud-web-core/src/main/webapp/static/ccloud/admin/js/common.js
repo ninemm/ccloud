50dba2054d38f3e3b677c54c52e77eb54882c62b
zcjj6 <zcjj6@DESKTOP-6KFDN9J>
2018-01-29 18:01:16 +0800

    1.第三方订单查询

--- ccloud-web-core/src/main/webapp/static/ccloud/admin/js/common.js
+++ ccloud-web-core/src/main/webapp/static/ccloud/admin/js/common.js
@@ -904,6 +904,19 @@ function unCheckParentNode(treeId,data){
 	}
 }
 
+function formatNumber(num, precision, separator) {
+    var parts;
+    if (!isNaN(parseFloat(num)) && isFinite(num)) {
+        num = Number(num);
+        num = (typeof precision !== 'undefined' ? num.toFixed(precision) : num).toString();
+        parts = num.split('.');
+        parts[0] = parts[0].toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1' + (separator || ','));
+
+        return parts.join('.');
+    }
+    return NaN;
+}
+
 
 Date.prototype.Format = function (fmt) { //author: meizz 
     var o = {
