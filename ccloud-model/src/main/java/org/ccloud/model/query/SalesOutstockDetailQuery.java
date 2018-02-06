0fe6a8b3a91ab781e963cb809aee7defefcb374f
yangliyang <948487671@qq.com>
2018-02-06 15:53:46 +0800

    打印增加财务成本价打印

--- ccloud-model/src/main/java/org/ccloud/model/query/SalesOutstockDetailQuery.java
+++ ccloud-model/src/main/java/org/ccloud/model/query/SalesOutstockDetailQuery.java
@@ -137,7 +137,7 @@ public class SalesOutstockDetailQuery extends JBaseQuery {
 		
 	}
 	
-	
+	//仓库销售价打印
 	public List<orderProductInfo> findPrintProductInfo(String outstockId) {
 		StringBuilder sqlBuilder = new StringBuilder(
 				" SELECT sod.outstock_id,sod.is_gift, sod.sell_product_id,sp.custom_name, p.big_unit, p.small_unit, p.convert_relate, sp.seller_id, sp.product_id, t1.valueName, cs.is_composite,sp.bar_code,sod.product_price,CONVERT( sod.product_price/p.convert_relate,decimal(18,2)) as small_price, ");
@@ -443,5 +443,44 @@ public class SalesOutstockDetailQuery extends JBaseQuery {
 		return true;
 	}
 	
+     	//财务成本价打印
+		public List<orderProductInfo> findFinancePrintProductInfo(String outstockId) {
+			StringBuilder sqlBuilder = new StringBuilder(
+					" SELECT sod.outstock_id,sod.is_gift, sod.sell_product_id,sp.custom_name, p.big_unit, p.small_unit, p.convert_relate, sp.seller_id, sp.product_id, t1.valueName, cs.is_composite,sp.bar_code,sp.cost as product_price,CONVERT( sp.cost/p.convert_relate,decimal(18,2)) as small_price, ");
+			sqlBuilder.append(" floor(sod.product_count/p.convert_relate) as bigCount,MOD(sod.product_count,p.convert_relate) as smallCount,sod.product_amount,sod.product_count,sod.id as salesOutDetaliId,cso.warehouse_id,sp.product_id as productId ");
+			sqlBuilder.append(" from `cc_sales_outstock_detail` sod ");
+			sqlBuilder.append(" LEFT JOIN cc_sales_outstock cso on cso.id = sod.outstock_id ");
+			sqlBuilder.append(" LEFT JOIN cc_sales_order_detail cs ON sod.order_detail_id = cs.id ");
+			sqlBuilder.append(" LEFT JOIN cc_seller_product sp ON sod.sell_product_id = sp.id ");
+			sqlBuilder.append(" LEFT JOIN cc_product p ON sp.product_id = p.id ");
+			sqlBuilder.append("LEFT JOIN  (SELECT sv.id, cv.product_set_id, GROUP_CONCAT(sv. NAME) AS valueName FROM cc_goods_specification_value sv ");
+			sqlBuilder.append("RIGHT JOIN cc_product_goods_specification_value cv ON cv.goods_specification_value_set_id = sv.id GROUP BY cv.product_set_id) t1 on t1.product_set_id = p.id ");
+			sqlBuilder.append(" WHERE sod.outstock_id = ? ");
+
+			List<Record> records = Db.find(sqlBuilder.toString(), outstockId);
+			List<orderProductInfo> orderProductInfos = new ArrayList<>();
+			for (Record record : records) {
+				orderProductInfo orderProductInfo = new orderProductInfo();
+				orderProductInfo.setProductName(record.getStr("custom_name"));//产品名称		
+				orderProductInfo.setBarCode(record.getStr("bar_code"));//条码
+				orderProductInfo.setBigUnit(record.getStr("big_unit"));//产品大单位
+				orderProductInfo.setSmallUnit(record.getStr("small_unit"));//产品小单位
+				orderProductInfo.setConvertRelate(record.getInt("convert_relate"));//换算关系
+				orderProductInfo.setBigPrice(record.getBigDecimal("product_price"));//大单位价格
+				orderProductInfo.setSmallPrice(record.getBigDecimal("small_price"));//小单位价格
+				orderProductInfo.setBigCount(record.getInt("bigCount"));
+				orderProductInfo.setSmallCount(record.getInt("smallCount"));
+				orderProductInfo.setIsgift(record.getInt("is_gift"));
+				orderProductInfo.setProductAmout(record.getBigDecimal("product_amount"));
+				orderProductInfo.setProductCount(record.getInt("product_count"));
+				orderProductInfo.setSellerProductId(record.getStr("sell_product_id"));
+				orderProductInfo.setOutStockId(record.getStr("outstock_id"));
+				orderProductInfo.setSalesOutDetaliId(record.getStr("salesOutDetaliId"));
+				orderProductInfo.setWareHouseId(record.getStr("warehouse_id"));
+				orderProductInfo.setProductId(record.getStr("productId"));
+				orderProductInfos.add(orderProductInfo);
+			}
+			 return orderProductInfos;
+		}
 	
 }
