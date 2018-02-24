4dfb2a5eb61817427c73c01c07ac6b6111b3e3f5
yangfan <15549459554@163.com>
2018-02-24 15:01:26 +0800

    通过图片获取定位地址

--- ccloud-commons/src/main/java/org/ccloud/utils/ParsingImageUtils.java
+++ ccloud-commons/src/main/java/org/ccloud/utils/ParsingImageUtils.java
@@ -1,38 +1,51 @@
 package org.ccloud.utils;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
 
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 //读取图片信息
 public class ParsingImageUtils {
 	
-	public  String parsingImage(String imgUrl) throws Exception, Exception{  
+	public Map<String, String> parsingImage(String imgUrl) throws Exception, Exception{  
 		byte[] readInputStream = readInputStream(imgUrl);
 		InputStream sbs = new ByteArrayInputStream(readInputStream); 
 		Metadata metadata = ImageMetadataReader.readMetadata(sbs);
-		String content="";
+		Map<String, String>map=new HashMap<>();
 		for (Directory directory : metadata.getDirectories()) {  
 			for (Tag tag : directory.getTags()) {  
 				String tagName = tag.getTagName();  //标签名
 				String desc = tag.getDescription(); //标签信息
 				if (tagName.equals("Date/Time Original")) {  
-					content=content+"拍摄时间: "+desc;
+					map.put("Time", desc);
 				}else if (tagName.equals("GPS Latitude")) {  
-					content=content+"  纬度 : "+pointToLatlong(desc);
-				} else if (tagName.equals("GPS Longitude")) {  
-					content=content+"  经度 : "+pointToLatlong(desc);
+					map.put("Latitude", pointToLatlong(desc));
+				} else if (tagName.equals("GPS Longitude")) { 
+					map.put("Longitude", pointToLatlong(desc));
 				}
 			}  
 		}
-		return content;  
+		if (map.size()!=3) {
+			return null;
+		}
+		double[] latlng = postBaidu(map.get("Longitude"), map.get("Latitude"));
+		map.put("Latitude", String.valueOf(latlng[0]));
+		map.put("Longitude", String.valueOf(latlng[1]));
+		return map;  
 	}  
 	
 	
@@ -69,7 +82,6 @@ public class ParsingImageUtils {
 		return outStream.toByteArray();
 	}
 	
-	
 	 /** 
 	 * 经纬度格式  转换为  度分秒格式 ,如果需要的话可以调用该方法进行转换
 	 * @param point 坐标点 
@@ -81,5 +93,62 @@ public class ParsingImageUtils {
 		Double miao = Double.parseDouble(point.substring(point.indexOf("'")+1, point.indexOf("\"")).trim());  
 		Double duStr = du + fen / 60 + miao / 60 / 60 ;  
 		return duStr.toString();  
-	}  
+	}
+	
+	 //GPS坐标转百度坐标
+	public static double[] postBaidu(String lng, String lat) {
+		double[] latlng = null;
+
+		URL url = null;
+		URLConnection connection = null;
+		try {
+			url = new URL("http://api.map.baidu.com/ag/coord/convert?from=0&to=4&x=" + lng + "&y="+ lat);
+			connection = url.openConnection();
+			connection.setConnectTimeout(1000);
+			connection.setReadTimeout(1000);
+			connection.setDoOutput(true);
+			OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream(), "utf-8");
+			out.flush();
+			out.close();
+
+			// 服务器的回应的字串，并解析
+			String sCurrentLine;
+			String sTotalString;
+			sCurrentLine = "";
+			sTotalString = "";
+			InputStream l_urlStream;
+			l_urlStream = connection.getInputStream();
+			BufferedReader l_reader = new BufferedReader(new InputStreamReader(l_urlStream));
+			while ((sCurrentLine = l_reader.readLine()) != null) {
+				if (!sCurrentLine.equals(""))
+					sTotalString += sCurrentLine;
+			}
+			// System.out.println(sTotalString);
+			sTotalString = sTotalString.substring(1, sTotalString.length() - 1);
+			// System.out.println(sTotalString);
+			String[] results = sTotalString.split("\\,");
+			if (results.length == 3) {
+				if (results[0].split("\\:")[1].equals("0")) {
+					String mapX = results[1].split("\\:")[1];
+					String mapY = results[2].split("\\:")[1];
+					mapX = mapX.substring(1, mapX.length() - 1);
+					mapY = mapY.substring(1, mapY.length() - 1);
+					mapX = new String(Base64.decode(mapX));
+					mapY = new String(Base64.decode(mapY));
+					// System.out.println(mapX);
+					// System.out.println(mapY);
+					latlng = new double[] { Double.parseDouble(mapX), Double.parseDouble(mapY) };
+				} else {
+					System.out.println("error != 0");
+				}
+			} else {
+				System.out.println("String invalid!");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("GPS转百度坐标异常！");
+		}
+		return latlng;
+	}
 }
