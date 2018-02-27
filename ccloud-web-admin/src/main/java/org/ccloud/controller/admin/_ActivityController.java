9a86998dc8253de3d3dd578a549bef6ffdcbb801
Larry <Larry@DESKTOP-QIDTT2S>
2018-02-27 17:29:58 +0800

    活动执行

--- ccloud-web-admin/src/main/java/org/ccloud/controller/admin/_ActivityController.java
+++ ccloud-web-admin/src/main/java/org/ccloud/controller/admin/_ActivityController.java
@@ -32,8 +32,10 @@ import org.ccloud.route.RouterMapping;
 import org.ccloud.route.RouterNotAllowConvert;
 import org.ccloud.utils.StringUtils;
 import org.ccloud.model.Activity;
+import org.ccloud.model.ActivityExecute;
 import org.ccloud.model.Dict;
 import org.ccloud.model.QyExpense;
+import org.ccloud.model.query.ActivityExecuteQuery;
 import org.ccloud.model.query.ActivityQuery;
 import org.ccloud.model.query.CustomerTypeQuery;
 import org.ccloud.model.query.DictQuery;
@@ -184,8 +186,29 @@ public class _ActivityController extends JBaseCRUDController<Activity> {
 		activity.setEndTime(edate);
 		activity.setCustomerType(customerTypes);
 		activity.saveOrUpdate();
+		int num = Integer.valueOf(getPara("num"));
+		List<ActivityExecute> activityExecutes = ActivityExecuteQuery.me().findbyActivityId(activity.getId());
+		if(activityExecutes.size()>0) {
+			for(ActivityExecute ae : activityExecutes) {
+				ActivityExecuteQuery.me().batchDelete(ae.getId());
+			}
+		}
+		if(num>0) {
+			for(int i = 0; i <num ; i++) {
+				if(getPara("orderList"+(i+1))==null){
+					continue;
+				}
+				ActivityExecute activityExecute = new ActivityExecute();
+				activityExecute.setId(StrKit.getRandomUUID());
+				activityExecute.setActivityId(activity.getId());
+				activityExecute.setOrderList(Integer.valueOf(getPara("orderList"+(i+1))));
+				activityExecute.setRemark(getPara("remark"+(i+1)));
+				activityExecute.save();
+			}
+		}
 		renderAjaxResultForSuccess();
 	}
+	
 	public void getCustomerTypeOptions() {
 		String id = getPara("id");
 		String DataArea = getSessionAttr(Consts.SESSION_DEALER_DATA_AREA);
@@ -274,4 +297,9 @@ public class _ActivityController extends JBaseCRUDController<Activity> {
 		return areaType;
 	}
 
+	public void getActivityExecute() {
+		String activityId = getPara("activityId");
+		List<ActivityExecute> activityExecuteList = ActivityExecuteQuery.me().findbyActivityId(activityId);
+		renderJson(activityExecuteList);
+	}
 }
