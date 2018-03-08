4fcfa468e831ef5197967a6ceb68e382ce0d6dce
WT <1427249946@qq.com>
2018-03-08 11:44:50 +0800

    产品列表和购物车页面

--- ccloud-model/src/main/java/org/ccloud/model/query/MemberJoinSellerQuery.java
+++ ccloud-model/src/main/java/org/ccloud/model/query/MemberJoinSellerQuery.java
@@ -16,6 +16,11 @@
 package org.ccloud.model.query;
 
 import java.util.LinkedList;
+import java.util.List;
+
+import com.jfinal.kit.StrKit;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 import org.ccloud.model.MemberJoinSeller;
 
 import com.jfinal.plugin.activerecord.Page;
@@ -65,6 +70,34 @@ public class MemberJoinSellerQuery extends JBaseQuery {
 			return deleteCount;
 		}
 		return 0;
+	}
+
+	public List<Record> findProductListForApp(String memberId, String keyword, String tag) {
+		StringBuilder fromBuilder = new StringBuilder(
+				" SELECT sp.id AS sell_product_id, sp.product_id, sp.custom_name, sp.store_count, sp.price, sp.cost, sp.account_price, sp.tags,"
+						+ " p.convert_relate, p.product_sn, p.big_unit, p.small_unit, p.description, t1.valueName,"
+						+ " g.`name` AS goodsName, g.product_image_list_store, gc.`id` AS categoryId, gc.`name` AS categoryName, gt.`id` as typeId, gt.`name` as typeName ");
+		fromBuilder.append(" FROM cc_seller_product sp JOIN cc_product p ON sp.product_id = p.id ");
+		fromBuilder.append(" LEFT JOIN (SELECT sv.id, cv.product_set_id, GROUP_CONCAT(sv.`name`) AS valueName FROM cc_goods_specification_value sv RIGHT JOIN cc_product_goods_specification_value cv ON cv.goods_specification_value_set_id = sv.id GROUP BY cv.product_set_id ) t1 ON t1.product_set_id = p.id ");
+		fromBuilder.append(" JOIN cc_goods g ON p.goods_id = g.id JOIN cc_goods_category gc ON g.goods_category_id = gc.id JOIN cc_goods_type gt on g.goods_type_id = gt.id ");
+		fromBuilder.append(" WHERE sp.is_enable = 1 AND sp.is_gift = 0");
+
+		LinkedList<Object> params = new LinkedList<Object>();
+
+		fromBuilder.append(" AND sp.seller_id IN (SELECT cmjs.seller_id FROM cc_member_join_seller cmjs WHERE cmjs.member_id = ? AND cmjs.status = ? ) ");
+		params.add(memberId);
+		params.add(1);
+
+		appendIfNotEmptyWithLike(fromBuilder, "sp.custom_name", keyword, params, false);
+
+		if (StrKit.notBlank(tag)) {
+			fromBuilder.append(" AND FIND_IN_SET(?, sp.tags)");
+			params.add(tag);
+		}
+
+		fromBuilder.append(" ORDER BY gc.`parent_id`, gc.`order_list`, gc.`id`, sp.order_list ");
+
+		return Db.find(fromBuilder.toString(), params.toArray());
 	}
 
 	
--- ccloud-web-member/src/main/java/org/ccloud/controller/member/ProductController.java
+++ ccloud-web-member/src/main/java/org/ccloud/controller/member/ProductController.java
@@ -13,18 +13,73 @@
  */
 package org.ccloud.controller.member;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.ImmutableMap;
+import com.jfinal.plugin.activerecord.Record;
 import org.ccloud.core.BaseFrontController;
+import org.ccloud.model.query.*;
 import org.ccloud.route.RouterMapping;
 
-@RouterMapping(url = "/")
+import java.io.Serializable;
+import java.util.*;
+
+@RouterMapping(url = "/member/product")
 public class ProductController extends BaseFrontController {
 
 	public void index() {
-
-
-
 		render("product.html");
 	}
+
+	public void productList() {
+		String memberId = "98ef315d484d435fa2845d1ed9762912";
+
+		String keyword = getPara("keyword");
+		String tag = getPara("tag");
+
+		List<Record> productList = MemberJoinSellerQuery.me().findProductListForApp(memberId, keyword, tag);
+
+		Set<String> tagSet = new LinkedHashSet<String>();
+
+		for (Record record : productList) {
+			String tags = record.getStr("tags");
+			if (tags != null) {
+				String[] tagArray = tags.split(",", -1);
+				for (String str : tagArray) {
+					tagSet.add(str);
+				}
+			}
+		}
+
+		Map<String, Collection<? extends Serializable>> map = ImmutableMap.of("productList", productList, "tags", tagSet);
+		renderJson(map);
+	}
+
+	public void shoppingCart() {
+		String memberId = "98ef315d484d435fa2845d1ed9762912";
+//		String sellerCode = getSessionAttr(Consts.SESSION_SELLER_CODE);
+
+		List<Record> productList = MemberJoinSellerQuery.me().findProductListForApp(memberId, "", "");
+
+		Map<String, Object> sellerProductInfoMap = new HashMap<String, Object>();
+		List<Map<String, Object>> sellerProductItems = new ArrayList<>();
+
+		for (Record record : productList) {
+			Map<String, Object> item = new HashMap<>();
+
+			String sellProductId = record.get("sell_product_id");
+			item.put("title", record.getStr("custom_name"));
+			item.put("value", sellProductId);
+
+			sellerProductItems.add(item);
+			sellerProductInfoMap.put(sellProductId, record);
+		}
+
+		setAttr("sellerProductInfoMap", JSON.toJSON(sellerProductInfoMap));
+		setAttr("sellerProductItems", JSON.toJSON(sellerProductItems));
+
+		render("shopping_cart.html");
+	}
+
 
 
 }
