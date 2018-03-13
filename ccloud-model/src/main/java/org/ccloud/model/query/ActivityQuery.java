e18a378ee2fc14e5c4378e669cda9af3c5aad8ce
yangfan <15549459554@163.com>
2018-03-13 14:30:34 +0800

    ..

--- ccloud-model/src/main/java/org/ccloud/model/query/ActivityQuery.java
+++ ccloud-model/src/main/java/org/ccloud/model/query/ActivityQuery.java
@@ -196,9 +196,9 @@ public class ActivityQuery extends JBaseQuery {
 		fromBuilder.append(" LEFT JOIN `user` u ON u.id=caa.biz_user_id");
 		fromBuilder.append(" LEFT JOIN cc_seller_customer csc ON csc.id=caa.seller_customer_id");
 		fromBuilder.append(" LEFT JOIN cc_customer cc ON cc.id = csc.customer_id");
-		fromBuilder.append(" LEFT JOIN(  SELECT ccv.seller_customer_id ,ccv.user_id ,COUNT(1) executeNum,aa.id FROM cc_customer_visit ccv LEFT JOIN cc_activity_apply aa ON aa.id = ccv.active_apply_id   WHERE aa.activity_id = '");
+		fromBuilder.append(" LEFT JOIN(  SELECT ccv.seller_customer_id ,ccv.user_id ,COUNT(1) executeNum FROM cc_activity_apply aa LEFT JOIN cc_customer_visit ccv ON aa.id = ccv.active_apply_id   WHERE aa.activity_id = '");
 		fromBuilder.append(id+"' AND aa.`status` in(1,4) GROUP BY ccv.seller_customer_id,ccv.user_id) t1 ON t1.seller_customer_id = csc.id and t1.user_id=caa.biz_user_id");
-		fromBuilder.append(" WHERE caa.id=t1.id and ca.id='"+id+"' AND caa.`status` in(1,4) ");
+		fromBuilder.append(" WHERE ca.id='"+id+"' AND caa.`status` in(1,4) ");
 		if (StrKit.notBlank(keyword)) {
 			fromBuilder.append(" and (cc.customer_name like '%"+keyword+"%' or u.realname like '%"+keyword+"%')");
 		}
