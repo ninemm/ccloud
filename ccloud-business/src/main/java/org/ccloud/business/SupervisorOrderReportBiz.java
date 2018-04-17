465f8c87416e2d15276e6288ce740b06d5c78db6
wally <wally@DESKTOP-VKTH3Q7>
2018-04-17 10:51:57 +0800

    1. 优化业务经理前台报表-BIZ; 2. 优化业务员所在部门业务员销售排行-BIZ; 3. 优化主管的业务员销售排行-BIZ;

--- ccloud-business/src/main/java/org/ccloud/business/SupervisorOrderReportBiz.java
+++ ccloud-business/src/main/java/org/ccloud/business/SupervisorOrderReportBiz.java
@@ -42,15 +42,53 @@ public class SupervisorOrderReportBiz {
 		if(records != null && records.size() > 0) {
 			Record record = null;
 			User user = null;
+			String realname = null;
+			String avatar = null;
 			for (Iterator<Record> iterator = records.iterator(); iterator.hasNext();) {
 				record = (Record) iterator.next();
 				user = UserQuery.me().findById(String.valueOf(record.getColumns().get("id")));
-				if(user != null) {
-					record.getColumns().put("realname", user.getRealname());
-					record.getColumns().put("avatar", user.getAvatar());
-				}
+				realname = user != null ? user.getRealname() : "";
+				avatar = user != null ? user.getAvatar() : "";
+				record.getColumns().put("realname", realname);
+				record.getColumns().put("avatar", avatar);
 				record = null;
 				user = null;
+				realname = null;
+				avatar = null;
+			}
+		}
+		return records;
+	}
+	
+	/**
+	 * 查询主管下业务员的订单销售排行
+	 * @param startDate：开始时间
+	 * @param endDate：结束时间
+	 * @param dayTag：天数标识
+	 * @param orderTag：订单标识
+	 * @param dataArea：主管的数据域
+	 * @param receiveType：收款类型
+	 * @return
+	 */
+	public List<Record> getOrderRankOfMySalesmen(String startDate, String endDate, String dayTag,
+			String orderTag, String dataArea, String receiveType) {
+		List<Record> records = SupervisorOrderReportQuery.me().getOrderRankOfMySalesmen(startDate, endDate, dayTag, orderTag, dataArea, receiveType);
+		if(records != null && records.size() > 0) {
+			Record record = null;
+			User user = null;
+			String realname = null;
+			String avatar = null;
+			for (Iterator<Record> iterator = records.iterator(); iterator.hasNext();) {
+				record = (Record) iterator.next();
+				user = UserQuery.me().findById(String.valueOf(record.getColumns().get("id")));
+				realname = user != null ? user.getRealname() : "";
+				avatar = user != null ? user.getAvatar() : "";
+				record.getColumns().put("realname", realname);
+				record.getColumns().put("avatar", avatar);
+				record = null;
+				user = null;
+				realname = null;
+				avatar = null;
 			}
 		}
 		return records;
