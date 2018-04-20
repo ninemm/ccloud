48e3a4cbbdff0e7f7ed1d151233efa31f1577680
chenxuebing2 <445943149@qq.com>
2018-04-20 20:03:39 +0800

    修正包不正确的问题

--- ccloud-cache/src/main/java/org/ccloud/cache/impl/J2Cache.java
+++ ccloud-cache/src/main/java/org/ccloud/cache/impl/J2Cache.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (c) 2015-2016, Eric Huang 黄鑫 (ninemm@qq.com).
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,73 +30,47 @@ import net.oschina.j2cache.CacheChannel;
 import net.oschina.j2cache.CacheObject;
 
 public class J2Cache implements ICache {
-	
+
 	private static CacheChannel cache;
-	
+
 	public static void init(CacheChannel cache) {
 		J2Cache.cache = cache;
 	}
-	
+
 	public static CacheChannel getCacheChannel() {
 		return cache;
 	}
-	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+
+	@SuppressWarnings({"rawtypes", "unchecked"})
 	@Override
 	public <T> T get(String cacheName, Object key) {
-		try {
-			CacheObject cacheObject = cache.get(cacheName, key.toString());
-			return cacheObject != null ? (T) cacheObject.getValue() : null;
-		} catch (IOException e) {
-			LogKit.error(e.toString(), e);
-		}
-		return null;
+		CacheObject cacheObject = cache.get(cacheName, key.toString());
+		return cacheObject != null ? (T) cacheObject.getValue() : null;
 	}
 
 	@Override
 	public void put(String cacheName, Object key, Object value) {
-		try {
-			cache.set(cacheName, key.toString(), (Serializable) value);
-		} catch (IOException e) {
-			LogKit.error(e.toString(), e);
-		}
+		cache.set(cacheName, key.toString(), value);
 	}
-	
+
 	public void put(String cacheName, Object key, Object value, int liveSeconds) {
-		try {
-			cache.set(cacheName, key.toString(), (Serializable) value, liveSeconds);
-		} catch (IOException e) {
-			LogKit.error(e.toString(), e);
-		}
-    }
+		cache.set(cacheName, key.toString(), value, liveSeconds);
+	}
 
 	@Override
 	public List<?> getKeys(String cacheName) {
-		try {
-			Collection<String> keys = cache.keys(cacheName);
-			return keys != null ? new ArrayList<String>(keys) : null;
-		} catch (IOException e) {
-			LogKit.error(e.toString(), e);
-		}
-		return null;
+		Collection<String> keys = cache.keys(cacheName);
+		return keys != null ? new ArrayList<String>(keys) : null;
 	}
 
 	@Override
 	public void remove(String cacheName, Object keys) {
-		try {
-			cache.evict(cacheName, keys.toString());
-		} catch (IOException e) {
-			LogKit.error(e.toString(), e);
-		}
+		cache.evict(cacheName, keys.toString());
 	}
 
 	@Override
 	public void removeAll(String cacheName) {
-		try {
-			cache.clear(cacheName);
-		} catch (IOException e) {
-			LogKit.error(e.toString(), e);
-		}
+		cache.clear(cacheName);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -111,18 +85,18 @@ public class J2Cache implements ICache {
 		}
 		return (T) value;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public <T> T get(String cacheName, Object key, IDataLoader dataLoader, int liveSeconds) {
-        if (liveSeconds <= 0) {
-            return get(cacheName, key, dataLoader);
-        }
+		if (liveSeconds <= 0) {
+			return get(cacheName, key, dataLoader);
+		}
 
-        Object data = get(cacheName, key);
-        if (data == null) {
-            data = dataLoader.load();
-            put(cacheName, key, data, liveSeconds);
-        }
-        return (T) data;
-    }
+		Object data = get(cacheName, key);
+		if (data == null) {
+			data = dataLoader.load();
+			put(cacheName, key, data, liveSeconds);
+		}
+		return (T) data;
+	}
 }
