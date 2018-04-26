b835ab552fe6b8bfb030670c8a70d009dd64b768
759132976 <gaochaodi@163.com>
2018-04-26 09:43:45 +0800

    1.jar包

---
+++ ccloud-web-activitys/src/main/java/model/vo/Expense.java
@@ -0,0 +1,188 @@
+package model.vo;
+
+public class Expense {
+    private String ExpenseID;
+    private String FlowID;
+    private String FlowNo;
+    private String FlowTypeID;
+    private String ActivityNo;
+    private String ExpenseName;
+    private String FranchiserCode;
+    private String BrandCode;
+    private String  ProvinceCode;
+    private String  CityCode;
+    private String ExpenseBeginDate;
+    private String ExpenseEndDate;
+    private String InputDay;
+    private String Memo;
+    private String InputAccountType;
+    private String InputPayType;
+    private String ApplyAmount;
+    private String OperateYear;
+    private String ApplyPersonNumber;
+    private String ApplyPersonName;
+    private String ApplyTime;
+    private String CreateTime;
+    private String ModifyTime;
+    private String Flag;
+    private String CommoditySortID;
+    private String SchemeState;
+    
+	public String getExpenseID() {
+		return ExpenseID;
+	}
+	public void setExpenseID(String expenseID) {
+		ExpenseID = expenseID;
+	}
+	public String getFlowID() {
+		return FlowID;
+	}
+	public void setFlowID(String flowID) {
+		FlowID = flowID;
+	}
+	public String getFlowNo() {
+		return FlowNo;
+	}
+	public void setFlowNo(String flowNo) {
+		FlowNo = flowNo;
+	}
+	public String getFlowTypeID() {
+		return FlowTypeID;
+	}
+	public void setFlowTypeID(String flowTypeID) {
+		FlowTypeID = flowTypeID;
+	}
+	public String getActivityNo() {
+		return ActivityNo;
+	}
+	public void setActivityNo(String activityNo) {
+		ActivityNo = activityNo;
+	}
+	public String getExpenseName() {
+		return ExpenseName;
+	}
+	public void setExpenseName(String expenseName) {
+		ExpenseName = expenseName;
+	}
+	public String getFranchiserCode() {
+		return FranchiserCode;
+	}
+	public void setFranchiserCode(String franchiserCode) {
+		FranchiserCode = franchiserCode;
+	}
+	public String getBrandCode() {
+		return BrandCode;
+	}
+	public void setBrandCode(String brandCode) {
+		BrandCode = brandCode;
+	}
+	public String getProvinceCode() {
+		return ProvinceCode;
+	}
+	public void setProvinceCode(String provinceCode) {
+		ProvinceCode = provinceCode;
+	}
+	public String getCityCode() {
+		return CityCode;
+	}
+	public void setCityCode(String cityCode) {
+		CityCode = cityCode;
+	}
+	public String getExpenseBeginDate() {
+		return ExpenseBeginDate;
+	}
+	public void setExpenseBeginDate(String expenseBeginDate) {
+		ExpenseBeginDate = expenseBeginDate;
+	}
+	public String getExpenseEndDate() {
+		return ExpenseEndDate;
+	}
+	public void setExpenseEndDate(String expenseEndDate) {
+		ExpenseEndDate = expenseEndDate;
+	}
+	public String getInputDay() {
+		return InputDay;
+	}
+	public void setInputDay(String inputDay) {
+		InputDay = inputDay;
+	}
+	public String getMemo() {
+		return Memo;
+	}
+	public void setMemo(String memo) {
+		Memo = memo;
+	}
+	public String getInputAccountType() {
+		return InputAccountType;
+	}
+	public void setInputAccountType(String inputAccountType) {
+		InputAccountType = inputAccountType;
+	}
+	public String getInputPayType() {
+		return InputPayType;
+	}
+	public void setInputPayType(String inputPayType) {
+		InputPayType = inputPayType;
+	}
+	public String getApplyAmount() {
+		return ApplyAmount;
+	}
+	public void setApplyAmount(String applyAmount) {
+		ApplyAmount = applyAmount;
+	}
+	public String getOperateYear() {
+		return OperateYear;
+	}
+	public void setOperateYear(String operateYear) {
+		OperateYear = operateYear;
+	}
+	public String getApplyPersonNumber() {
+		return ApplyPersonNumber;
+	}
+	public void setApplyPersonNumber(String applyPersonNumber) {
+		ApplyPersonNumber = applyPersonNumber;
+	}
+	public String getApplyPersonName() {
+		return ApplyPersonName;
+	}
+	public void setApplyPersonName(String applyPersonName) {
+		ApplyPersonName = applyPersonName;
+	}
+	public String getApplyTime() {
+		return ApplyTime;
+	}
+	public void setApplyTime(String applyTime) {
+		ApplyTime = applyTime;
+	}
+	public String getCreateTime() {
+		return CreateTime;
+	}
+	public void setCreateTime(String createTime) {
+		CreateTime = createTime;
+	}
+	public String getModifyTime() {
+		return ModifyTime;
+	}
+	public void setModifyTime(String modifyTime) {
+		ModifyTime = modifyTime;
+	}
+	public String getFlag() {
+		return Flag;
+	}
+	public void setFlag(String flag) {
+		Flag = flag;
+	}
+	public String getCommoditySortID() {
+		return CommoditySortID;
+	}
+	public void setCommoditySortID(String commoditySortID) {
+		CommoditySortID = commoditySortID;
+	}
+	public String getSchemeState() {
+		return SchemeState;
+	}
+	public void setSchemeState(String schemeState) {
+		SchemeState = schemeState;
+	}
+    
+}
---
+++ ccloud-web-activitys/src/main/java/model/vo/ExpensesDetail.java
@@ -0,0 +1,138 @@
+package model.vo;
+
+import com.jfinal.kit.StrKit;
+
+public class ExpensesDetail {
+	private String ExpenseDetailID;//费用投入明细ID
+	private String ExpenseID;//费用投入主ID
+	private String FlowID;//流程ID
+	private String FlowTypeID;//流程分类
+	private String CostType;//费用类型
+	private String CommodityCode;//产品编码
+	private String ChannelID;//渠道ID
+	private String Quantity;//申请数量
+	private String ShowType;//展示形式
+	private String ApplyAmount;//申请总金额
+	private String CreateTime;//创建日期
+	private String ModifyTime;//最后修改日期
+	private String Flag;//数据有效状态(1:有效，0:无效)
+	
+	private String item1;
+	private String item2;
+	private String item3;
+	private String item4;
+	
+	public void setItemInfo() {
+		if (StrKit.notBlank(this.CommodityCode)) {
+			setItem2(this.CommodityCode);
+		} else {
+			setItem2(this.ShowType);
+		}
+		setItem3(this.Quantity);
+		setItem4(this.ApplyAmount);
+	}
+	
+	public String getItem1() {
+		return item1;
+	}
+	public void setItem1(String item1) {
+		this.item1 = item1;
+	}
+	public String getItem2() {
+		return item2;
+	}
+	public void setItem2(String item2) {
+		this.item2 = item2;
+	}
+	public String getItem3() {
+		return item3;
+	}
+	public void setItem3(String item3) {
+		this.item3 = item3;
+	}
+	public String getItem4() {
+		return item4;
+	}
+	public void setItem4(String item4) {
+		this.item4 = item4;
+	}
+	public String getCommodityCode() {
+		return CommodityCode;
+	}
+	public void setCommodityCode(String commodityCode) {
+		CommodityCode = commodityCode;
+	}
+	public String getChannelID() {
+		return ChannelID;
+	}
+	public void setChannelID(String channelID) {
+		ChannelID = channelID;
+	}
+	public String getQuantity() {
+		return Quantity;
+	}
+	public void setQuantity(String quantity) {
+		Quantity = quantity;
+	}
+	public String getShowType() {
+		return ShowType;
+	}
+	public void setShowType(String showType) {
+		ShowType = showType;
+	}
+	public String getExpenseDetailID() {
+		return ExpenseDetailID;
+	}
+	public void setExpenseDetailID(String expenseDetailID) {
+		ExpenseDetailID = expenseDetailID;
+	}
+	public String getExpenseID() {
+		return ExpenseID;
+	}
+	public void setExpenseID(String expenseID) {
+		ExpenseID = expenseID;
+	}
+	public String getFlowID() {
+		return FlowID;
+	}
+	public void setFlowID(String flowID) {
+		FlowID = flowID;
+	}
+	public String getFlowTypeID() {
+		return FlowTypeID;
+	}
+	public void setFlowTypeID(String flowTypeID) {
+		FlowTypeID = flowTypeID;
+	}
+	public String getCostType() {
+		return CostType;
+	}
+	public void setCostType(String costType) {
+		CostType = costType;
+	}
+	public String getApplyAmount() {
+		return ApplyAmount;
+	}
+	public void setApplyAmount(String applyAmount) {
+		ApplyAmount = applyAmount;
+	}
+	public String getCreateTime() {
+		return CreateTime;
+	}
+	public void setCreateTime(String createTime) {
+		CreateTime = createTime;
+	}
+	public String getModifyTime() {
+		return ModifyTime;
+	}
+	public void setModifyTime(String modifyTime) {
+		ModifyTime = modifyTime;
+	}
+	public String getFlag() {
+		return Flag;
+	}
+	public void setFlag(String flag) {
+		Flag = flag;
+	}
+	
+}
