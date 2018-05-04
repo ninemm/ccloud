6b69f0073727faf727e152fcf7159e960e4298f9
chenxuebing2 <445943149@qq.com>
2018-05-04 16:01:15 +0800

    完善区域模块

--- ccloud-bi2/ccloud-bi2-templates/src/main/webapp/templates/datacenter/js/pc_comm.js
+++ ccloud-bi2/ccloud-bi2-templates/src/main/webapp/templates/datacenter/js/pc_comm.js
@@ -17,9 +17,16 @@ var brandId = $.cookie(Utils.brandIdCache);
 
 
 $(function () {
+	if(cityName.length != 0) {
+		renderCityMap({"name": cityName}, curProvName);
+	} else if(provName.length != 0){
+		renderProvMap({"name": curProvName}, true);
+	} else {
+		renderChinaMap(true);
+	}
 	initDatetimepicker('#start-date', 'yyyy-MM-dd', 2, 4, 2);
 	initDatetimepicker('#end-date', 'yyyy-MM-dd', 2, 4, 2);
-	$("[data-id='" + brandId + "']").click();
+	$("[data-id='" + brandId + "']").addClass('red-button').siblings().removeClass('red-button');
 });
 
 // 时间切换选择, 如近一天，近一周等
@@ -114,13 +121,29 @@ function initDatetimepicker(fieldId, format, minView, maxView, startView) {
 	});
 }
 
-function selectDealer(selectDataArea,obj,url) {
+$("#searchInput").on("input", function() {
+	if ($("#searchInput").val() == '') {
+		$(".weui-cell").show();
+	}
+
+	$(".weui-cell").hide().filter(":contains("+ $("#searchInput").val().trim() +")").show();
+});
+
+$("#searchClear").on("click", function() {
+	$(".weui-cell").show();
+});
+
+$("#searchCancel").on("click", function() {
+	$(".weui-cell").show();
+});
+
+function selectDealer(selectDataArea,obj) {
 	Utils.loading();
 	dataArea = selectDataArea;
 	$.cookie(Utils.dataAreaCache, dataArea, { expires: 1 });
 	$("#dealerName").text($(obj).find(".weui-cell__hd").eq(0).text() + '汇总信息');
 	$.ajax({
-		url:url,
+		url:CPATH + '/pc/selectDealer',
 		type:"post",
 		data:{dataArea: dataArea},
 		dataType:"json"
@@ -156,5 +179,379 @@ function selectDealer(selectDataArea,obj,url) {
 			renderChinaMap(true);
 		}
 
+	});
+}
+
+var prov_data = [];
+var city_data = [];
+var country_data = [];
+
+//回到中国地图界面
+function goBackChinaMap() {
+
+	renderChinaMap(true);
+
+	provName = '';
+	cityName = '';
+	countryName = '';
+	$.cookie(Utils.provCacheName, provName, { expires: 7 });
+	$.cookie(Utils.cityCacheName, cityName, { expires: 7 });
+	$.cookie(Utils.countryCacheName, countryName, { expires: 7 });
+
+}
+
+//回到省界面
+function goBackProvMap() {
+
+	cityName = '';
+	countryName = '';
+	$.cookie(Utils.provCacheName, provName, { expires: 7 });
+	$.cookie(Utils.cityCacheName, cityName, { expires: 7 });
+	$.cookie(Utils.countryCacheName, countryName, { expires: 7 });
+
+	curProvName = strKit.convertProvName(provName);
+	renderProvMap({"name": curProvName}, true);
+
+}
+
+//点击地图上的省
+function renderProvMap (result, isRender) {
+	$('#city').css('visibility','hidden');
+	cityName = '';
+	countryName = '';
+	var maxData = 0;
+
+	if(result.name == '北京' || result.name == '上海' || result.name == '天津' || result.name == '台湾' || result.name == '重庆'){
+		provName = result.name + '市';
+	} else {
+		provName = result.name + '省';
+	}
+	curProvName = strKit.convertProvName(provName);
+
+	$.cookie(Utils.provCacheName, provName, { expires: 7 });
+	$.cookie(Utils.cityCacheName, cityName, { expires: 7 });
+	$.cookie(Utils.countryCacheName, countryName, { expires: 7 });
+
+	$.ajax({
+		url:CPATH + "/biSales/queryMapData",
+		type:"post",
+		data:{"provName": provName, "cityName": '', countryName: '', startDate: startDate, endDate: endDate, dataArea: dataArea, brandId: brandId},
+		dataType:"json"
+	}).done(function(data) {
+		city_data = [];
+		for(var i = 0; i < data.length; i++) {
+			city_data.push({"name": data[i].city_name, "value": data[i].realAmount});
+			if(data[i].realAmount > maxData) maxData = data[i].realAmount;
+		}
+
+		//setTimeout(function () {
+		$('#prov_view').text(result.name);
+		$('#prov').css('visibility', 'visible');
+
+		if (isRender) {
+			$('#china_map').css('display','none');
+			$('#prov_map').css('display','block');
+			$('#city_map').css('display','none');
+		}
+		//}, 500);
+
+		var _provMap = document.getElementById('prov_map');
+		var provChart = echarts.init(_provMap);
+		$(_provMap).width(1000);
+		// 选择省的单击事件
+		var selectProv = result.name.toString();
+
+		provChart.showLoading({
+			text: '加载中...',
+			effect: 'whirling'
+		});
+
+		// 从json获取地图数据
+		$.get(CTPATH + '/json/' + cityMap[selectProv] + '/'+cityMap[selectProv] + '.json', function (data) {
+			echarts.registerMap(selectProv, data);
+
+			provChart.hideLoading();
+			provChart.resize();
+
+			provChart.setOption({
+				tooltip: {
+					trigger: 'item',
+					textStyle: {fontSize: 9},
+					formatter: function loadData(result){
+						if(isNaN(result.value)) return result.name + '<br />总金额: 0';
+						else return result.name+'<br />总金额:'+result.value + '万元';
+					}
+				},
+				visualMap:{
+					min: 0,
+					max: maxData+1,
+					splitNumber: 0,
+					text: ['高','低'],
+					realtime: false,
+					selectedMode: false,
+					realtime: false,
+					show: true,
+					itemWidth: 20,
+					itemHeight: 120
+				},
+				series: [{
+					type: 'map',
+					map: selectProv,                  // 要和echarts.registerMap（）中第一个参数一致
+					roam: true,
+					scaleLimit: { min: 1, max: 4 },   // 缩放
+					mapLocation:{
+						y:60
+					},
+					itemSytle:{
+						emphasis:{label:{show:false}}
+					},
+					label: {
+						normal: {
+							show: true
+						},
+						emphasis: {
+							show: true
+						}
+					},
+					data : city_data.reverse()
+				}]
+			});
+
+			provChart.on('click', function(rel){
+				renderCityMap(rel, selectProv);
+			});
+		});
+	});
+	initData();
+}
+
+//点击地图上的市
+function renderCityMap(rel, selectProv) {
+	var maxData = 0;
+	cityName = rel.name;
+
+	$.cookie(Utils.provCacheName, provName, { expires: 7 });
+	$.cookie(Utils.cityCacheName, cityName, { expires: 7 });
+	$.cookie(Utils.countryCacheName, countryName, { expires: 7 });
+
+	$.ajax({
+		url:CPATH + "/biSales/queryMapData",
+		type:"post",
+		data:{"provName": provName, "cityName": cityName, "countryName": '', startDate: startDate, endDate: endDate, dataArea: dataArea, brandId: brandId},
+		dataType:"json"
+	}).done(function(data) {
+		country_data = [];
+		for(var i = 0; i < data.length; i++)
+		{
+			country_data.push({"name": data[i].country_name, "value": data[i].realAmount});
+			if (data[i].realAmount > maxData) maxData = data[i].realAmount;
+		}
+
+		setTimeout(function () {
+			function contains(arr, obj) {
+				var i = arr.length;
+				while (i--) {
+					if (arr[i] === obj) {
+						return true;
+					}
+				}
+				return false;
+			}
+
+			var arr = new Array('北京','上海','天津','台湾','重庆');
+
+			if(contains(arr, selectProv) == false) {
+
+				$('#prov_view').text(selectProv);
+				$('#prov').css('visibility','visible');
+				$('#city_view').text(rel.name);
+				$('#city').css('visibility','visible');
+
+				$('#china_map').css('display','none');
+				$('#prov_map').css('display','none');
+				$('#city_map').css('display','block');
+
+			} else {
+				$('#china_map').css('display','none');
+				$('#prov_map').css('display','block');
+				$('#city_map').css('display','none');
+			}
+		}, 500);
+
+		// 选择市的单击事件
+		var selectCity = rel.name;
+
+		// 调取后台数据
+		countryMapRender(selectProv, selectCity, maxData);
+	});
+	initData();
+}
+
+// 城市地图(市)
+function countryMapRender(selectProv, selectCity, maxData) {
+	var _cityMap = document.getElementById('city_map');
+	var cityChart = echarts.init(_cityMap);
+	$(_cityMap).width(1000);
+
+	cityChart.showLoading({
+		text: '加载中...',
+		effect: 'whirling'
+	});
+
+	$.get(CTPATH + '/json/'+cityMap[selectProv]+'/'+cityMap[selectCity]+'.json', function (data) {
+
+		echarts.registerMap(selectCity, data);
+		cityChart.hideLoading();
+		cityChart.resize();
+
+		cityChart.setOption({
+			tooltip: {
+				trigger: 'item',
+				textStyle: {fontSize: 9},
+				formatter: function loadData(result) {
+					if(isNaN(result.value)) return result.name + '<br />总金额: 0';
+					else return result.name+'<br />总金额:'+result.value + '万元';
+				}
+			},
+			visualMap:{
+				min: 0,
+				max: maxData+1,
+				splitNumber: 0,
+				text: ['高','低'],
+				realtime: false,
+				selectedMode: false,
+				realtime: false,
+				show: true,
+				itemWidth: 20,
+				itemHeight: 120
+			},
+			series: [{
+				type: 'map',
+				map: selectCity ,//要和echarts.registerMap（）中第一个参数一致
+				roam: true,
+				scaleLimit: { min: 1, max: 4 },//缩放
+				mapLocation: {
+					y:60
+				},
+				itemSytle: {
+					emphasis: {label: {show: false} }
+				},
+				label: {
+					normal: {
+						show: true
+					},
+					emphasis: {
+						show: true
+					}
+				},
+				data: country_data.reverse()
+			}]
+		});
+
+		/*cityChart.on('click', function(rel) {
+			setTimeout(function () {
+			}, 500);
+		});*/
+	});
+}
+
+//中国地图渲染
+function renderChinaMap(isRender) {
+	$('#prov').css('visibility','hidden');
+	$('#city').css('visibility','hidden');
+	var maxData = 0;
+
+	$.ajax({
+		url: CPATH + "/biSales/queryMapData",
+		type: "post",
+		data: {startDate: startDate, endDate: endDate, dataArea: dataArea, brandId: brandId},
+		dataType:"json"
+	}).done(function(data) {
+		prov_data = [];
+		for(var i = 0; i < data.length; i++) {
+			prov_data.push({
+				"name": data[i].prov_name.substring(0, data[i].prov_name.length -1),
+				"value": data[i].realAmount
+			});
+
+			if (data[i].realAmount > maxData)
+				maxData = data[i].realAmount;
+		}
+
+		if (isRender) {
+			//setTimeout(function () {
+			$('#china_map').css('display','block');
+			$('#city_map').css('display','none');
+			$('#prov_map').css('display','none');
+			//}, 100);
+		}
+
+		var _chinaMap = document.getElementById('china_map');
+		$(_chinaMap).width(1000);
+		var chart = echarts.init(_chinaMap);                     // 在id为china_map的dom元素中显示地图
+		chart.showLoading({
+			text: '加载中...',
+			effect: 'whirling'
+		});
+
+		$.get(CTPATH + '/json/china.json', function (mapJson) {
+
+			echarts.registerMap('china', mapJson);
+			chart.hideLoading();
+			chart.resize();
+
+			chart.setOption({
+				tooltip: {
+					trigger: 'item',
+					textStyle: {fontSize: 9},
+					formatter: function (result){
+						if(isNaN(result.value))
+							return result.name + '<br />总金额: 0';
+						else
+							return result.name + '<br />总金额:' + result.value + '万元';
+					}
+				},
+				visualMap: {
+					min: 0,
+					max: maxData + 1,
+					splitNumber: 0,
+					text: ['高','低'],
+					realtime: false,
+					selectedMode: false,
+					realtime: false,
+					show: true,
+					itemWidth: 20,
+					itemHeight: 120
+				},
+				series: [{
+					type: 'map',
+					map: 'china',                   // 要和echarts.registerMap（）中第一个参数一致
+					roam: true,
+					scaleLimit: { min: 1, max: 4},  // 缩放
+					mapLocation:{
+						y:60
+					},
+					itemSytle: {
+						emphasis: {label: {show: false}}
+					},
+					label: {
+						normal: {
+							show: true,
+							textStyle: {fontSize: 9},
+						},
+						emphasis: {
+							show: true,
+							textStyle: {fontSize: 9},
+						}
+					},
+					data: prov_data.reverse()
+				}]
+			});
+
+			chart.on('click', function(result) {
+				renderProvMap(result, true);
+			});
+		});
+		initData();
 	});
 }
